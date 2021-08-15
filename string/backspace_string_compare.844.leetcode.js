34037458d1c104eeee32c7098a1a569e62694636
nishant-gloify <nishantindia18@gmail.com>
2021-08-15 21:55:43 +0530

    back space string solution

---
+++ string/backspace_string_compare.844.leetcode.js
@@ -0,0 +1,47 @@
+const string1 = "ab#z"
+const string2 = "az#z"
+
+var backspaceCompare = function(S, T) {
+    let p1 = S.length - 1, p2 = T.length - 1;
+    
+    while(p1 >= 0 || p2 >= 0) {
+        if(S[p1] === "#" || T[p2] === "#") {
+            if(S[p1] === "#") {
+                let backCount = 2;
+                
+                while(backCount > 0) {
+                    p1--;
+                    backCount--;
+                    
+                    if(S[p1] === "#") {
+                        backCount += 2;
+                    }
+                }
+            }
+            
+            if(T[p2] === "#") {
+                let backCount = 2;
+                
+                while(backCount > 0) {
+                    p2--;
+                    backCount--;
+                    
+                    if(T[p2] === "#") {
+                        backCount += 2;
+                    }
+                }
+            }
+        } else {
+            if(S[p1] !== T[p2]) {
+                return false;
+            } else {
+                p1--;
+                p2--;
+            }
+        }
+    }
+    
+    return true;
+};
+
+console.log(backspaceCompare(string1, string2));
